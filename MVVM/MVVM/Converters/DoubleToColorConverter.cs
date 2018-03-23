4dccad1635cbd1e506cb7335af84d2be734271cb
Eduardo Rosas <lalorosas@outlook.com>
2018-03-23 11:12:03 -0600

    IValueConverter implemented

---
+++ MVVM/MVVM/Converters/DoubleToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MVVM.Converters
+{
+    //! added using Xamarin.Forms;
+    public class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double result = (double)value;
+            if (result >= 0)
+                return Color.Green;
+            return Color.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)0;
+        }
+    }
+}
